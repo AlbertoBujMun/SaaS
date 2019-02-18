abda3447f90ce413d84af80235cdd26d1d8e6afb
jultrocor <j.troncosocorrea@gmail.com>
2019-02-18 17:24:02 +0100

    Fixed commit error

--- Acme-Explorer/api/models/actorModel.js
+++ Acme-Explorer/api/models/actorModel.js
@@ -5,7 +5,7 @@ var Schema = mongoose.Schema;
  * @swagger
  * tags:
  *   name: Actor
- *   description: 
+ *   description:
  */
 /**
  * @swagger
@@ -50,96 +50,102 @@ var Schema = mongoose.Schema;
  *          type: string
  */
 
-var ActorSchema = new Schema({
+var ActorSchema = new Schema(
+  {
     name: {
-        type: String,
-        required: "Kindly enter the actor name"
+      type: String,
+      required: "Kindly enter the actor name"
     },
     surname: {
-        type: String,
-        required: "Kindly enter the actor surname"
+      type: String,
+      required: "Kindly enter the actor surname"
     },
     email: {
-        type: String,
-        required: "Kindly enter the actor email",
-        unique: true,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Please fill a valid email address"
-        ]
+      type: String,
+      required: "Kindly enter the actor email",
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address"
+      ]
     },
     password: {
-        type: String,
-        minlength: 5,
-        required: "Kindly enter the actor password"
+      type: String,
+      minlength: 5,
+      required: "Kindly enter the actor password"
     },
     preferredLanguage: {
-        type: String,
-        default: "en"
+      type: String,
+      default: "en"
     },
     phoneNumber: {
-        type: String,
-        required: "Kindly enter the phone number"
+      type: String,
+      required: "Kindly enter the phone number"
     },
     address: {
-        type: String
+      type: String
     },
-    role: [{
+    role: [
+      {
         type: String,
         required: "Kindly enter the user role(s)",
         enum: ["MANAGER", "EXPLORER", "ADMINISTRATOR", "SPONSOR"]
-    }],
+      }
+    ],
     deleted: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     banned: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false
     },
     created: {
-        type: Date,
-        default: Date.now
+      type: Date,
+      default: Date.now
     },
     banner: {
-        data: Buffer,
-        type: String,
-        required: "Kindly enter a sponsor banner"
+      type: String,
+      data: Buffer,
+      contentType: String,
+      validate: [isSponsor, "Kindly enter a sponsor banner"]
     },
     link: {
-        type: String,
-        required: "Kindly enter a sponsor link"
+      type: String,
+      validate: [isSponsor, "Kindly enter a sponsor link"]
     }
-}, { strict: false });
+  },
+  { strict: false }
+);
 
 function isSponsor() {
-    return this.role === "SPONSOR";
+  return this.role === "SPONSOR";
 }
 
 ActorSchema.pre("save", function(callback) {
-    var actor = this;
-    // Break out if the password hasn't changed
-    if (!actor.isModified("password")) return callback();
+  var actor = this;
+  // Break out if the password hasn't changed
+  if (!actor.isModified("password")) return callback();
 
-    // Password changed so we need to hash it
-    bcrypt.genSalt(5, function(err, salt) {
-        if (err) return callback(err);
+  // Password changed so we need to hash it
+  bcrypt.genSalt(5, function(err, salt) {
+    if (err) return callback(err);
 
-        bcrypt.hash(actor.password, salt, function(err, hash) {
-            if (err) return callback(err);
-            actor.password = hash;
-            callback();
-        });
+    bcrypt.hash(actor.password, salt, function(err, hash) {
+      if (err) return callback(err);
+      actor.password = hash;
+      callback();
     });
+  });
 });
 
 ActorSchema.methods.verifyPassword = function(password, cb) {
-    bcrypt.compare(password, this.password, function(err, isMatch) {
-        console.log("verifying password in actorModel: " + password);
-        if (err) return cb(err);
-        console.log("iMatch: " + isMatch);
-        cb(null, isMatch);
-    });
+  bcrypt.compare(password, this.password, function(err, isMatch) {
+    console.log("verifying password in actorModel: " + password);
+    if (err) return cb(err);
+    console.log("iMatch: " + isMatch);
+    cb(null, isMatch);
+  });
 };
 
 module.exports = mongoose.model("Actors", ActorSchema);
