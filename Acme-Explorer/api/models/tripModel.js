bcdd02fc412155edf1d20e7bd097509592c91b8d
rafavermo@gmail.com <rafael.vergara@sngular.team>
2019-03-25 13:47:33 +0100

    Generador de ticker

--- Acme-Explorer/api/models/tripModel.js
+++ Acme-Explorer/api/models/tripModel.js
@@ -1,29 +1,28 @@
 "use strict";
 var mongoose = require("mongoose");
-var dateFormat = require("dateformat");
 var Schema = mongoose.Schema;
 
-var TripStageSchema = new Schema(
-  {
+const generate = require('nanoid/generate');
+const dateFormat = require('dateformat');
+
+var TripStageSchema = new Schema({
     title: {
-      type: String,
-      required: "Title required"
+        type: String,
+        required: "Title required"
     },
     description: {
-      type: String,
-      required: "Stage description required"
+        type: String,
+        required: "Stage description required"
     },
     price: {
-      type: Number,
-      min: 0
+        type: Number,
+        min: 0
     },
     deleted: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     }
-  },
-  { strict: false }
-);
+}, { strict: false });
 
 /**
  * @swagger
@@ -68,70 +67,63 @@ var TripStageSchema = new Schema(
  *       manager:
  *         type: object
  */
-var TripSchema = new Schema(
-  {
+var TripSchema = new Schema({
     ticker: {
-      type: String,
-      unique: true
+        type: String,
+        unique: true
     },
     title: {
-      type: String,
-      required: "Kindly enter the title"
+        type: String,
+        required: "Kindly enter the title"
     },
     description: {
-      type: String,
-      required: "Kindly enter the description"
+        type: String,
+        required: "Kindly enter the description"
     },
-    requirement: [
-      {
+    requirement: [{
         type: String,
         required: "Kindly enter the requirement(s)"
-      }
-    ],
+    }],
     startDate: {
-      type: Date,
-      required: "Kindly enter the start date of the trip"
+        type: Date,
+        required: "Kindly enter the start date of the trip"
     },
     endDate: {
-      type: Date,
-      required: "Kindly enter the end date of the trip"
+        type: Date,
+        required: "Kindly enter the end date of the trip"
     },
-    photo: [
-      {
+    photo: [{
         data: Buffer,
         contentType: String
-      }
-    ],
+    }],
     cancelationReason: {
-      type: String
+        type: String
     },
     created: {
-      type: Date,
-      default: Date.now
+        type: Date,
+        default: Date.now
     },
     public: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     },
     deleted: {
-      type: Boolean,
-      default: false
+        type: Boolean,
+        default: false
     },
     manager: {
-      type: Schema.Types.ObjectId,
-      required: "Manager id is required"
+        type: Schema.Types.ObjectId,
+        required: "Manager id is required"
     },
     tripStage: [TripStageSchema]
-  },
-  { strict: false }
-);
+}, { strict: false });
 
-TripSchema.virtual("price").get(function () {
-  var i = 0;
-  this.tripStage.forEach(function (x) {
-    i = i + x.price
-  })
-  return i;
+TripSchema.virtual("price").get(function() {
+    var i = 0;
+    this.tripStage.forEach(function(x) {
+        i = i + x.price
+    })
+    return i;
 });
 
 TripSchema.index({ "tripStage.price": 1 });
@@ -141,25 +133,16 @@ TripSchema.index({ created: 1 });
 TripSchema.index({ title: "text", description: "text", ticker: "text" });
 
 // Execute before each trip.save() call
-TripSchema.pre("save", function (callback) {
-  var new_trip = this;
-  var day = dateFormat(new Date(), "yymmdd");
+TripSchema.pre("save", function(callback) {
+    var new_trip = this;
+    var day = dateFormat(new Date(), "yymmdd");
 
-  var generated_ticker = [day, rand_str()].join(
-    "-"
-  );
-  new_trip.ticker = generated_ticker;
-  callback();
+    var generated_ticker = [day, generate("ABCDEFGHIJKLMNPQRSTUVWXYZ", 4)].join(
+        "-"
+    );
+    new_trip.ticker = generated_ticker;
+    callback();
 });
 
-function rand_str() {
-  const list = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
-  var res = "";
-  for (var i = 0; i < 4; i++) {
-    var rnd = Math.floor(Math.random() * list.length);
-    res = res + list.charAt(rnd);
-  }
-  return res;
-}
 
 module.exports = mongoose.model("Trips", TripSchema);
--- Acme-Explorer/api/v2/routes/dataWarehouseRoutes.js
+++ Acme-Explorer/api/v2/routes/dataWarehouseRoutes.js
@@ -1,29 +1,28 @@
 'use strict';
-module.exports = function (app) {
-    var dataWareHouse = require('../controllers/dataWareHouseController');
 
-
+module.exports = function(app) {
+    var dataWareHouse = require('../controllers/dataWarehouseController');
     /**
-   * Get a list of all indicators or post a new computation period for rebuilding
-   * RequiredRole: Administrator
-   * @section dataWareHouse
-   * @type get post
-   * @url /dataWareHouse
-   * @param [string] rebuildPeriod
-   * 
-  */
-    app.route('/dataWareHouse')
+     * Get a list of all indicators or post a new computation period for rebuilding
+     * RequiredRole: Administrator
+     * @section dataWareHouse
+     * @type get post
+     * @url /dataWareHouse
+     * @param [string] rebuildPeriod
+     * 
+     */
+    app.route('/v2/dataWareHouse')
         .get(dataWareHouse.list_all_indicators)
         .post(dataWareHouse.rebuildPeriod);
 
     /**
-    * Get a list of last computed indicator
-    * RequiredRole: Administrator
-    * @section dataWareHouse
-    * @type get
-    * @url /dataWareHouse/latest
-    *
-    */
+     * Get a list of last computed indicator
+     * RequiredRole: Administrator
+     * @section dataWareHouse
+     * @type get
+     * @url /dataWareHouse/latest
+     *
+     */
     app.route('/v2/dataWareHouse/latest')
         .get(dataWareHouse.last_indicator);
 };
